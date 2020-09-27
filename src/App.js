c2d35019d8b977f3339ddd5d393966ab7ca6c2e7
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-27 16:04:16 -0400

    added parallax

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
 
 // components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
@@ -7,6 +10,7 @@ import TitleMessage from "./components/title-message/title-message.component";
 import About from "./pages/about/about.component";
 
 import "./App.css";
+import { Parallax } from "react-parallax";
 
 const App = () => {
   return (
@@ -14,7 +18,20 @@ const App = () => {
       <MyNavbar />
       <MyCarousel />
       <TitleMessage />
-      <About />
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <About />
+            </Fade>
+          </Container>
+        </Parallax>
+      </div>
     </div>
   );
 };
--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -4,21 +4,89 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Profile from "../../assets/img/profile/profile.webp";
+import Button from "react-bootstrap/Button";
+
+import "./about.style.css";
 
 function About() {
   return (
     <div id="about">
-      <h1>About Me</h1>
-      <Container>
-        <Row>
-          <Col xs={12} md={6}>
-            <Row>
-              <Image src={Profile} thumbnail />
-            </Row>
-          </Col>
-          <Col xs={12} md={6}></Col>
-        </Row>
-      </Container>
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">About Me</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2">
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className="align-items-start p-2 my-details rounded">
+                Hi! I am<strong>&nbsp;Anthony Bergen.</strong>
+                <br />
+                A software engineer proficient in front-end and back-end web
+                development.
+                <br />
+                <br />
+                Additonally, I have over ten years of executive sales and
+                managerial experience, across a diverse list of industries
+                including cybersecurity, instrumentation engineering and
+                automation, telecommunications, and logistics.
+                <br />
+                <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/1WuMCwg68WfNHswutbGghRH2Df2c-ySr3/view"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/adbergen"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="www.linkedin.com/in/anthonybergen"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }

cae0a157e41a382a8a2984bff82bb2958780f18e
Anthony Bergen <anthonydbergen@gmail.com>
2020-10-05 23:20:59 -0400

    added skills icons

---
+++ build/precache-manifest.3fb8415d6bc1e0be2763e797bc275233.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457aa2e8ddba80b8f66f827df0c64873",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba46699f22df83546fbc",
+    "url": "/portfolio/static/css/main.4f2ff749.chunk.css"
+  },
+  {
+    "revision": "09386f153390ce79c35c",
+    "url": "/portfolio/static/js/2.99697361.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.99697361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba46699f22df83546fbc",
+    "url": "/portfolio/static/js/main.c193c8ec.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "44753d8e6e7c64bb5361b5f56112b547",
+    "url": "/portfolio/static/media/auth0.44753d8e.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/portfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c4237eef2af224ba27fb8768cf8ed307",
+    "url": "/portfolio/static/media/employee-directory.c4237eef.webp"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/portfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/portfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/portfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "dafe5587223fa3782f39797a9b6efa77",
+    "url": "/portfolio/static/media/logow.dafe5587.webp"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "25bd805ffbd0a06888275f123015c4ad",
+    "url": "/portfolio/static/media/mongoDB.25bd805f.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/portfolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "898915f9f4b8e2dec5733d3613eaf7ff",
+    "url": "/portfolio/static/media/profile.898915f9.webp"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/portfolio/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/portfolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "4568635a5ed302ec84d8371ff86b7f4e",
+    "url": "/portfolio/static/media/read-your-movie.4568635a.webp"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/portfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/portfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "2cb52e9ac1cf2cf80097bef3bec7675d",
+    "url": "/portfolio/static/media/the-gaming-network.2cb52e9a.webp"
+  }
+]);
